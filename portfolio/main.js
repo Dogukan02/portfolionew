5cc6567064663df4d96dd6561bcc72726d00d575
Dogukan02 <92163897+Dogukan02@users.noreply.github.com>
2023-11-23 15:14:04 +0300

    Add files via upload

---
+++ portfolio/main.js
@@ -0,0 +1,10 @@
+var typed= new typed(".text",{
+    strings:["Frontend Developer","Yazılımcı","Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+
+
+
+})
