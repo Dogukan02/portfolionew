231df3a519b35d4876c18ab98a5a06a09a00f7df
Dogukan02 <92163897+Dogukan02@users.noreply.github.com>
2023-11-27 09:29:46 +0300

    Add files via upload

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,10 +1,12 @@
-var typed= new typed(".text",{
-    strings:["Frontend Developer","Yazılımcı","Web Developer"],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
+document.addEventListener('DOMContentLoaded', function () {
+    var options = {
+        strings: ["Web Developer", "Full-stack Developer", "Front-end developer" ,  "Bilgisayar Mühendisi"],
+        typeSpeed: 50,   // typing speed in milliseconds
+        backSpeed: 30,    // backspacing speed in milliseconds
+        startDelay: 500,  // delay before starting to type
+        backDelay: 500,   // delay before starting to backspace
+        loop: true        // loop the animation infinitely
+    };
 
-
-
-})
+    var typed = new Typed('.text', options);
+});
